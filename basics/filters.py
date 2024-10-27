40d7e48a31ca7c5eaa25ec01ed4ec1c06f1b86af
Peter Saunderson <peteasa@gmail.com>
2024-10-27 20:48:25 +0000

    added measurement for the transfer function

--- basics/filters.py
+++ basics/filters.py
@@ -3,6 +3,13 @@
 import numpy as np
 import pyrpl
 
+def characterise_transfer_function(data, frequencies, condition):
+    idxs = np.where(condition)
+    fminidx_hi = idxs[0].min()
+    fminidx_lo = fminidx_hi - 1 if 0 < fminidx_hi else fminidx_hi
+
+    return fminidx_lo, fminidx_hi
+
 if __name__ == '__main__':
     HOSTNAME = 'rp-f0bd75'
     CONFIG = 'basic.filters'
@@ -11,19 +18,24 @@ if __name__ == '__main__':
 
     with p.iirs.pop('user') as iir:
         unity = False
+        lowpass = True
+        highpass = False
         stop_freq = 5e6
         gain = 1e-9
+        loops = 8
         TC = None # alternative is to specify the time constant: for example 1us
         if TC is None:
-            # alternative is to specify the cut off frequency: for example 1e6 Hz
-            fc = 1.59235e5
-        else:
+            fc = 1.5e5
+        elif TC is None:
             fc = 1 / (2 * np.pi * TC)
 
+        samples = round(125e6 / (loops * fc))
+        fc = 125e6 / (loops * samples)
         # reduce the number of loops to increase the sampling rate
-        # choose a suitable divisor based on the type of filter and the cutoff frequency selected
-        sampling_rate_adjustment = 1.0 / 160.0
+        # choose a suitable divisor
+        sampling_rate_adjustment = 1.0 / samples
         loops = round(sampling_rate_adjustment * 125e6 / fc)
+        print('loops: {:0.3f} samples: {:0.0f}'.format(loops, 1.0 / sampling_rate_adjustment))
         if 1023 < loops:
             loops = 1023
             print('frequency / time constant not possible - adjusted')
@@ -37,17 +49,16 @@ if __name__ == '__main__':
 
         print('filter cutoff frequency: {:0.3f} time constant: {:0.3f} us'.format(fc, TC * 1e6))
 
-        if True:
+        if lowpass:
             # low pass filter at f
             stop_freq = 3e7
-            zeros = [ ]
-            poles = [ omegac * complex( -1, 0 ) ]
-            print('poles: {}'.format(poles))
-        elif True:
+            zeros = np.array([], dtype = np.complex128)
+            poles = np.array([ omegac * complex( -1, 0 ) ], dtype = np.complex128)
+        elif highpass:
             # high pass filter at f
             stop_freq = 3e7
-            zeros = [ omegac * complex( 0, 0 ) ]
-            poles = [ omegac * complex( -1, 0 ) ]
+            zeros = np.array([ omegac * complex( -0.00001 / omegac, 0 ) ], dtype = np.complex128)
+            poles = np.array([ omegac * complex( -1, 0 ) ], dtype = np.complex128)
         elif False:
             # 3rd order butterworth filter
             stop_freq = 3e7
@@ -56,20 +67,19 @@ if __name__ == '__main__':
             end = 180 - start
             th = (end - start) / (nps - 1)
             angle = start
-            poles = []
+            poles = np.array([], dtype = np.complex128)
             for p in range(nps):
                 print('angle: {}'.format(angle))
                 imag = np.cos(angle * np.pi / 180)
                 if abs(omegac * imag) < 1.0:
                     imag = 0
 
-                poles.append(omegac * complex( -np.sin(angle * np.pi / 180),
-                                              imag ) )
+                poles = np.append(poles, omegac * complex( - np.sin(angle * np.pi / 180),
+                                               imag ) )
 
                 angle = angle + th # +0.000000001
 
-            print('poles: {}'.format(poles))
-            zeros = [ ]
+            zeros = np.array([], dtype = np.complex128)
         elif False:
             # notch filter at f
             angle = 68
@@ -80,19 +90,19 @@ if __name__ == '__main__':
                                       np.sin(angle * np.pi / 180) )
             n_pole = omegac * complex( -np.cos(angle * np.pi / 180),
                                       -np.sin((angle + 0.00001) * np.pi / 180) )
-            zeros = [ p_zero, n_zero ]
-            poles = [ p_pole, n_pole ]
+            zeros = np.array([ p_zero, n_zero ], dtype = np.complex128)
+            poles = np.array([ p_pole, n_pole ], dtype = np.complex128)
         else:
-            # band-pass filter at f
+            # band-pass filter at fc
             nps = 7
-            loops = 2 * nps * 125e6 / f
+            loops = 2 * nps * 125e6 / fc
             th = 180 / (nps - 1)
             angle = 0
             pole = omegac * complex( 0, 1 )
-            poles = []
+            poles = np.array([], dtype = np.complex128)
             for p in range(nps):
                 print('angle: {}'.format(angle))
-                poles.append(pole + omegac * complex( -np.sin(angle * np.pi / 180),
+                poles = np.append(poles, pole + omegac * complex( -np.sin(angle * np.pi / 180),
                                                      np.cos(angle * np.pi / 180) ) / 32)
 
                 angle = angle + th
@@ -100,13 +110,12 @@ if __name__ == '__main__':
             angle = 0.000001
             pole = omegac * complex( 0, -1 )
             for p in range(nps):
-                poles.append(pole + omegac * complex( -np.sin(angle * np.pi / 180),
+                poles = np.append(poles, pole + omegac * complex( -np.sin(angle * np.pi / 180),
                                                      np.cos(angle * np.pi / 180) ) / 32)
 
                 angle = angle + th
 
-            print('poles: {}'.format(poles))
-            zeros = [ ]
+            zeros = np.array([], dtype = np.complex128)
         if unity:
             iir._setup_unity()
         else:
@@ -114,6 +123,8 @@ if __name__ == '__main__':
                       zeros=zeros, poles=poles,
                       loops=loops)
 
+        print('poles: {}'.format([po / omegac for po in poles]))
+        print('zeros: {}'.format([ze / omegac for ze in zeros]))
         print('Filter sampling frequency: {:0.3f} MHz cut off frequency: {} MHz'.format(125./iir.loops, fc / 1e6))
 
         # useful diagnostic functions
@@ -145,7 +156,37 @@ if __name__ == '__main__':
         # retrieve designed transfer function
         designdata = iir.transfer_function(na.frequencies)
 
-        # plot the design data
+        if lowpass:
+            phases = np.angle(tf, deg = True)
+            fminpidx, fmaxpidx = characterise_transfer_function(phases, na.frequencies, phases < -45)
+            dbs = 20 * np.log10(np.abs(tf))
+            dbs = dbs - dbs.max()
+            fmindidx, fmaxdidx = characterise_transfer_function(dbs, na.frequencies, dbs < -3)
+            print('f between: {:0.0f} and {:0.0f} dbs: {:0.3f} and {:0.3f} phase: {:0.3f} and {:0.3f}'.format(
+                na.frequencies[fminpidx], na.frequencies[fmaxpidx],
+                dbs[fminpidx], dbs[fmaxpidx],
+                phases[fminpidx], phases[fmaxpidx]))
+            print('f between: {:0.0f} and {:0.0f} dbs: {:0.3f} and {:0.3f} phase: {:0.3f} and {:0.3f}'.format(
+                na.frequencies[fmindidx], na.frequencies[fmaxdidx],
+                dbs[fmindidx], dbs[fmaxdidx],
+                phases[fmindidx], phases[fmaxdidx]))
+        elif highpass:
+            phases = np.angle(tf, deg = True)
+            phases = phases - phases[0]
+            fminpidx, fmaxpidx = characterise_transfer_function(phases, na.frequencies, phases < -45)
+            dbs = 20 * np.log10(np.abs(tf))
+            dbs = dbs - dbs.max()
+            fmindidx, fmaxdidx = characterise_transfer_function(dbs, na.frequencies, -3 < dbs)
+            print('f between: {:0.0f} and {:0.0f} dbs: {:0.3f} and {:0.3f} phase: {:0.3f} and {:0.3f}'.format(
+                na.frequencies[fminpidx], na.frequencies[fmaxpidx],
+                dbs[fminpidx], dbs[fmaxpidx],
+                phases[fminpidx], phases[fmaxpidx]))
+            print('f between: {:0.0f} and {:0.0f} dbs: {:0.3f} and {:0.3f} phase: {:0.3f} and {:0.3f}'.format(
+                na.frequencies[fmindidx], na.frequencies[fmaxdidx],
+                dbs[fmindidx], dbs[fmaxdidx],
+                phases[fmindidx], phases[fmaxdidx]))
+
+        # plot the design data and the measured response
         import matplotlib
         matplotlib.rcParams['figure.figsize'] = (10, 6)
 
